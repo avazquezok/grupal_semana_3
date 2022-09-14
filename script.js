bdd7842868c23183c4b412e18f5a86bd15153333
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.4>
2022-09-14 19:37:31 -0300

    Terminado?

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ async function mostrar_galaxia() {
         const respuesta = await fetch("https://images-api.nasa.gov/search?q=" + galaxia.value)
         const data = await respuesta.json()
       console.log(data)
-        for (imagen of data.collection.items) {
+        for (let imagen of data.collection.items) {
             card_galaxia.innerHTML += `
-       <div class="col">
-       <div class="card h-100">
-         <img src="${imagen.links[0].href}" class="card-img-top" alt="${galaxia.value}">
-         <div class="card-body">
+       <div class="col size">
+       <div class="card h-100 ">
+         <img src="${imagen.links[0].href}" class="card-img-top imageSize" alt="${galaxia.value}">
+         <div class="card-body overflow-auto">
            <h5 class="card-title">${imagen.data[0].title}</h5>
            <p class="card-text"> ${imagen.data[0].description}</p>
          </div>
@@ -29,9 +29,9 @@ async function mostrar_galaxia() {
      console.log(error)
     }  
 }
+
 boton.addEventListener("click", (e) => {
     e.preventDefault()
     card_galaxia.innerHTML= ""
     mostrar_galaxia()
-
 })
